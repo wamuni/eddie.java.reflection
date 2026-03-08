1bb63f6d7d71f10e4cc37edafa83ba437b320398
wamuni <boshi.pan@outlook.com>
2026-03-08 23:52:06 +0000

    demo class for the reflection examples

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,28 @@
 package org.example;
 
+import java.lang.reflect.Field;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         int field = User.publicStaticField;
         System.out.println(field);
         User.myPublicStaticMethod();
+
+        User user = new User("Eddie", 30);
+        System.out.println(user.name);
+        user.myPublicMethod();
+
+        Class<User> userClass = User.class;
+        // 这里使用通配符，因为类对象的类必须要在runtime时才能确定，在编译时确定不了
+        Class<?> userClazz = user.getClass();
+
+        Class<?> clazz = Class.forName("org.example.User");
+
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field f: fields) {
+            System.out.println(f.getName());
+        }
     }
 }
--- src/main/java/org/example/Message.java
+++ src/main/java/org/example/Message.java
@@ -1,15 +1,6 @@
 package org.example;
 
-public class Message {
-    private final String content;
-
-    public Message(String content) {
-        this.content = content;
-    }
-
-    public String getContent() {
-        return this.content;
-    }
+public record Message(String content) {
 
     public void printMessage() {
         System.out.println("Message: " + this.content);
