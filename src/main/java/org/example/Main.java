03564c4ce2b254d3c60a4465159c79ed2a45c0f2
wamuni <boshi.pan@outlook.com>
2026-03-09 22:29:25 +0000

    some fun example about reflection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Field;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
         int field = User.publicStaticField;
         System.out.println(field);
         User.myPublicStaticMethod();
@@ -24,5 +24,27 @@ public class Main {
         for (Field f: fields) {
             System.out.println(f.getName());
         }
+
+        // 可以通过名字来找相应的feild
+        Field fieldByName = userClazz.getDeclaredField("name");
+        System.out.println(fieldByName.getType());
+
+        // 对于有泛型类型声明的字段field
+        // 如果name传错了，那么会报错，NoSuchFieldException
+        Field fieldByGeneric = userClass.getDeclaredField("comments");
+        // 只获取类型会只返回相应的类型，会进行泛型擦除
+        System.out.println(fieldByGeneric.getType());
+        System.out.println(fieldByGeneric.getGenericType());
+
+        // 通过反射来获取静态field和方法
+        Class<?> staticClazz = Class.forName("org.example.User");
+        Field staticField = staticClazz.getDeclaredField("publicStaticField");
+        // 因为获取的静态字段值，所以不需要一个具体的实例，因此可以通过null object来获取
+        // 但是需要处理异常
+        System.out.println(staticField.get(null));
+        staticField = staticClazz.getDeclaredField("privateStaticField");
+        // 在获取似有静态字段时，因为时私有的，所以需要获取权限
+        staticField.setAccessible(true);
+        System.out.println(staticField.get(null));
     }
 }
